e3ac2954c82d691c5de4e3396a9f82aebf75e5b4
Lior Kummer <lior@sneakybugs.com>
2022-04-12 19:29:37 +0300

    Add Python language server to neovim role

--- roles/neovim/templates/init.lua
+++ roles/neovim/templates/init.lua
@@ -28,7 +28,7 @@ cmp.setup({
 
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-local servers = { 'tsserver', 'gopls', 'dockerls', 'yamlls' }
+local servers = { 'tsserver', 'gopls', 'dockerls', 'yamlls', 'pyright' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     capabilities = capabilities
