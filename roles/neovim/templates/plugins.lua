7d79a49935cb6bd32fe8983d55144755890212c4
Lior Kummer <lior@sneakybugs.com>
2022-06-19 21:04:23 +0000

    Add Trouble to neovim role

--- roles/neovim/templates/plugins.lua
+++ roles/neovim/templates/plugins.lua
@@ -64,6 +64,30 @@ return require('packer').startup(function()
   -- Snippets for many languages.
   use "rafamadriz/friendly-snippets"
 
+  -- Diagnostics viewer.
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup()
+      vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
+        {silent = true, noremap = true}
+      )
+      vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>",
+        {silent = true, noremap = true}
+      )
+      vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>",
+        {silent = true, noremap = true}
+      )
+      vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>",
+        {silent = true, noremap = true}
+      )
+      vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",
+        {silent = true, noremap = true}
+      )
+    end
+  }
+
   -- Go laungauge integration.
   use {
     'ray-x/go.nvim',
