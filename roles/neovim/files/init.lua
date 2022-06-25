cb4dd0652a32970e9cfd57b7f86488782afbf09d
Lior Kummer <lior@sneakybugs.com>
2022-06-25 23:40:38 +0000

    Add LSP hover popup in NeoVim

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -6,3 +6,17 @@ vim.opt.clipboard = 'unnamedplus'
 vim.opt.number = true
 
 vim.api.nvim_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', {noremap = true})
+
+-- Show LSP hover when holding the cursor for 500ms.
+vim.opt.updatetime = 500
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  -- Prevent focusing on the hover popup.
+  -- See https://github.com/neovim/nvim-lspconfig/issues/1037
+  focusable = false,
+  border = "rounded"
+})
+vim.api.nvim_create_autocmd('CursorHold', {
+  callback = function()
+    vim.lsp.buf.hover()
+  end
+})
