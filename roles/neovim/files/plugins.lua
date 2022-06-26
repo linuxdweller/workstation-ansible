b3a0ae3c84ffa1935a9d34fd3b8b57e68ed948a9
Lior Kummer <lior@sneakybugs.com>
2022-06-26 00:00:41 +0000

    Add jumping to specific line with Telescope in NeoVim

--- roles/neovim/files/plugins.lua
+++ roles/neovim/files/plugins.lua
@@ -143,7 +143,52 @@ return require('packer').startup(function()
         { "nvim-telescope/telescope.nvim" }
     },
     config = function()
+      local telescope_actions = require('telescope.actions')
+      local telescope_actions_state = require('telescope.actions.state')
+      require('telescope').setup({
+        pickers = {
+          -- Jump to a line number when picking files.
+          -- Search for filename:80 to jump directly to line 80.
+          -- See https://gitter.im/nvim-telescope/community?at=6113b874025d436054c468e6
+          find_files = {
+            on_input_filter_cb = function(prompt)
+              local find_colon = string.find(prompt, ":")
+              if find_colon then
+                local ret = string.sub(prompt, 1, find_colon - 1)
+                vim.schedule(function()
+                  local prompt_bufnr = vim.api.nvim_get_current_buf()
+                  local picker = telescope_actions_state.get_current_picker(prompt_bufnr)
+                  local lnum = tonumber(prompt:sub(find_colon + 1))
+                  if type(lnum) == "number" then
+                    local win = picker.previewer.state.winid
+                    local bufnr = picker.previewer.state.bufnr
+                    local line_count = vim.api.nvim_buf_line_count(bufnr)
+                    vim.api.nvim_win_set_cursor(win, { math.max(1, math.min(lnum, line_count)), 0 })
+                  end
+                end)
+                return { prompt = ret }
+              end
+            end,
+            attach_mappings = function()
+              telescope_actions.select_default:enhance {
+                post = function()
+                  -- if we found something, go to line
+                  local prompt = telescope_actions_state.get_current_line()
+                  local find_colon = string.find(prompt, ":")
+                  if find_colon then
+                    local lnum = tonumber(prompt:sub(find_colon + 1))
+                    vim.api.nvim_win_set_cursor(0, { lnum, 0 })
+                  end
+                end,
+              }
+              return true
+            end,
+          },
+        }
+      })
+
       require("telescope").load_extension("yaml_schema")
+
       vim.api.nvim_set_keymap("n", "<leader>fy", "<cmd>Telescope yaml_schema<cr>",
         {silent = true, noremap = true}
       )
