5fd497fad08e8622c2719c34691e71c5e553eefa
Lior Kummer <lior@sneakybugs.com>
2022-07-03 00:20:36 +0000

    Add null-ls to NeoVim config
    Add null-ls and configure it with almost any linter or formatter I
    regularly use.

--- roles/neovim/files/plugins.lua
+++ roles/neovim/files/plugins.lua
@@ -200,6 +200,34 @@ return require('packer').startup(function()
     end
   }
 
+  use {
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.code_actions.gitsigns,
+          null_ls.builtins.diagnostics.ansiblelint,
+          null_ls.builtins.diagnostics.eslint,
+          null_ls.builtins.diagnostics.flake8,
+          null_ls.builtins.diagnostics.golangci_lint,
+          null_ls.builtins.diagnostics.hadolint,
+          null_ls.builtins.diagnostics.mypy,
+          null_ls.builtins.diagnostics.pylint,
+          null_ls.builtins.diagnostics.stylelint,
+          -- Is it actually needed with TSServer?
+          -- null_ls.builtins.diagnostics.tsc,
+          null_ls.builtins.formatting.black,
+          null_ls.builtins.formatting.eslint,
+          null_ls.builtins.formatting.gofmt,
+          null_ls.builtins.formatting.goimports,
+          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.formatting.stylelint
+        }
+      })
+    end
+  }
+
   -- File browser.
   use {
     "luukvbaal/nnn.nvim",
