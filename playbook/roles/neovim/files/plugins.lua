7f8ddf63eedb12224cd8dbb63a3611cc9b8f91ca
Lior Kummer <lior@sneakybugs.com>
2023-02-11 12:56:48 +0200

    Refactor mini.surround to nvim-surround
    Add nvim-surround config with default options.
    Update the lazy.nvim lockfile.
    
    It works very differently which will require some getting used to, but
    it offers a lot more features compared to mini.surround.

--- playbook/roles/neovim/files/plugins.lua
+++ playbook/roles/neovim/files/plugins.lua
@@ -245,18 +245,12 @@ return {
         delay = 0
       })
       require("mini.pairs").setup()
-      require("mini.surround").setup({
-        mappings = {
-          add = "<leader>sa", -- Add surrounding
-          delete = "<leader>sd", -- Delete surrounding
-          find = "<leader>sf", -- Find surrounding (to the right)
-          find_left = "<leader>sF", -- Find surrounding (to the left)
-          highlight = "<leader>sh", -- Highlight surrounding
-          replace = "<leader>sr", -- Replace surrounding
-          update_n_lines = "<leader>sn", -- Update `n_lines`
-        },
-      })
     end
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    config = true
   },
   {
     "lukas-reineke/indent-blankline.nvim",
