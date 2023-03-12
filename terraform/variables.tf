2dcfe32b2f486baaf1c3309760a1277f4beb3627
Lior Kummer <lior@sneakybugs.com>
2023-03-12 20:39:04 +0200

    Fix SSH auth and refactor to remote TF state

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,8 +11,4 @@ variable "proxmox_target_node" {
 variable "cloud_init_public_keys" {
   description = "SSH public keys to add with Cloud Init."
   type        = string
-  default     = <<EOF
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICmTzPVmwo0Q7txYnDkD2ubmRxLUBP1MB5x5j8+v0hK8 lior-workstation
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEGeVyeRpSkg/WaathpdgE93EuvM+0BysjFnprrxDR9O provisioning
-EOF
 }
