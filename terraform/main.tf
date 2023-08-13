aa15e69a096456660e928daeab8145a71ecaa70c
Lior Kummer <lior@sneakybugs.com>
2023-08-14 02:40:54 +0300

    Fix problems with Debian 12

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,13 +9,13 @@ module "workstation" {
 
   proxmox_api_url     = var.proxmox_api_url
   proxmox_target_node = var.proxmox_target_node
-  proxmox_template    = "debian-11.6.0-1"
+  proxmox_template    = "debian-12.1.0-1"
 
   name                   = "workstation-${random_uuid.machine_name.result}"
   description            = "Dynamically cloned workstation."
   on_boot                = true
-  memory                 = 4096
-  cores                  = 4
+  memory                 = 8192
+  cores                  = 8
   disk_pool              = "local-lvm"
   disk_size              = "32G"
   cloud_init_public_keys = var.cloud_init_public_keys
