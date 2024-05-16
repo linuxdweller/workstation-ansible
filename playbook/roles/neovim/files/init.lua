5ef72e622d59876161f922114baba25d01635d32
Lior Kummer <lior@sneakybugs.com>
2024-05-16 20:27:37 +0300

    feat: remove nvim mouse support and auto LSP hover
    Remove mouse support enabled by 0.10.0 update.
    Remove automatic LSP hover because a new default mapping was added for
    it `K`.

--- playbook/roles/neovim/files/init.lua
+++ playbook/roles/neovim/files/init.lua
@@ -3,7 +3,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup('plugins')
 
 -- Disable mouse mode.
-vim.opt.mouse = null
+vim.opt.mouse = ''
 
 -- Use normal clipboard.
 vim.opt.clipboard = 'unnamedplus'
@@ -17,25 +17,11 @@ vim.opt.undolevels = 10000
 vim.api.nvim_set_keymap('n', '<leader>rn', '', {noremap = true, callback = vim.lsp.buf.rename})
 
 -- Show LSP hover when holding the cursor for 500ms.
-vim.opt.updatetime = 500
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
   -- Prevent focusing on the hover popup.
   -- See https://github.com/neovim/nvim-lspconfig/issues/1037
   focusable = false,
   border = 'rounded'
-})
-vim.api.nvim_create_autocmd('CursorHold', {
-  callback = function()
-    -- Only call hover() if any attached LSP client supports hover.
-    clients = vim.lsp.buf_get_clients()
-    for _, client in pairs(clients) do
-      -- Ignore null-ls because it does not provide hover but is configured as if it does.
-      if client.name ~= 'null-ls' and client.server_capabilities.hoverProvider then
-        vim.lsp.buf.hover()
-        return
-      end
-    end
-  end
 })
 
 -- Statusline
