257b451065c5dd71894b52c9f865b34c3897a6ae
Amit Friedman <amit.kummer@gmail.com>
2025-01-02 20:48:45 +0200

    Revert "feat: improve NeoVim autocompletion performance"
    This reverts commit 76fbd63d42439a63df2a4213ca7bdf877dae7102.

--- playbook/roles/neovim/files/plugins.lua
+++ playbook/roles/neovim/files/plugins.lua
@@ -2,11 +2,11 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      "saghen/blink.cmp"
+      "hrsh7th/nvim-cmp"
     },
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      local capabilities = require("blink.cmp").get_lsp_capabilities()
       local servers = {
         "ansiblels",
         "astro",
@@ -55,22 +55,46 @@ return {
     end
   },
   {
-    "saghen/blink.cmp",
-    lazy = false,
-    dependencies = { "rafamadriz/friendly-snippets" },
-    version = "v0.*",
-    opts = {
-      keymap = {
-        preset = "super-tab"
-      },
-      completion = {
-        documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 0,
-          update_delay_ms = 0
-        }
-      }
-    }
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-vsnip",
+      "hrsh7th/vim-vsnip",
+      "hrsh7th/vim-vsnip-integ",
+      "rafamadriz/friendly-snippets"
+    },
+    config = function()
+      vim.opt.completeopt = {"menu", "menuone", "noselect"}
+
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end
+        },
+        mapping = cmp.mapping.preset.insert({
+          -- Recommended keymap.
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          -- Use tab to select next item.
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item()
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp"},
+          { name = "vsnip" }
+        }, {
+          { name = "buffer" }
+        })
+      })
+    end
   },
   {
     "folke/trouble.nvim",
@@ -222,11 +246,7 @@ return {
     name = "catppuccin",
     config = function()
       vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
-      require("catppuccin").setup({
-        integrations = {
-          blink_cmp = true
-        }
-      })
+      require("catppuccin").setup()
       vim.api.nvim_command("colorscheme catppuccin")
     end
   },
